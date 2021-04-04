c0f32f08ba746af0be0d6b008f4cd3b05051d1e5
박성호 <psh@psh-Mac.local>
2021-04-03 22:04:49 -0700

    second progress My PortFolio

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -1,35 +1,52 @@
 const express = require('express');
 const timeout = require('connect-timeout');
-const bodyParser = require('body-parser');
 const app = express();
 
 // 포트 설정 
 app.set('port', process.env.PORT || 3500);
+
+
+
 
 // 라우터 불러오기
 
 
 
 // Top level Modules
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(timeout(2000));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
 
-/* 시간 초과 요청 처리
-var timeout = require('connect-timeout'); //express v4
-
-app.use(timeout(120000));
-app.use(haltOnTimedout);
-
-function haltOnTimedout(req, res, next){
-  if (!req.timedout) next();
-}
-
-*/
 
 // 시간초과 처리
-// app.use(timeout('3s'));
+    function haltOnTimedout(req, res, next){
+        if (!req.timedout) next();
+        // timedout 프로퍼티가 없어야만, 다음 모듈로 넘긴다.
+        // 만약 여기서 timedout 프로퍼티가 존재한다면,
+        // next();를 만나지 못해서 넘어가지 못한다.
+        // 즉 여기서, timedout 가 걸려 버린다.
+    }
+    
+    // 시간 초과 Test
+    app.use((req, res, next) => {
+        console.log("시간 초과 TEST 진입");
+        setTimeout(()=>{
+            if(req.timedout){
+                // req.에timedout 프로퍼티가 있다면,
+                // 바로 다음 모듈로 넘겨서 결국 에러 핸들러 까지 가게 한다.
+                next();
+            }else{
+                // 만약 timedout 프로퍼티가 없다면,
+                // 성공 반응을 보낸다.
+                res.send('success');
+            }
+        }, 3000); // 현재 타임아웃 기준이 2초이다. 
+        // 따라서 여기서 2초 이전으로 하면 성공하고 2초 이상으로하면 요청초과된다.
+    });
+    //app.use(haltOnTimedout);
+  
 
 
 // 사용할 모듈
@@ -75,9 +92,12 @@ app.use((req, res)=>{
 
 
 // 에러 처리
-app.use(()=>{
+app.use((err, req, res, next)=>{
     console.log('Server Error!!!!!!!!!!!');
-    res.end('500! Server Error');
+    if(req.timedout){
+        res.end('503!! Timed Out');
+    }
+    res.end('500!! Server Error');
 });
 
 
