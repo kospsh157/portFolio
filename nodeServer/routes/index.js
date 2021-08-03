4260e114a34b729aa1bd67015269956ce98e6e8b
SungHo Park <kospsh157@gmail.com>
2021-08-03 10:25:36 -0700

    Clear Unnecessary Files

--- nodeServer/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-router.get('/', express.static('/Users/psh/Desktop/portFolio/nodeServer/public/index.html'));
-
-module.exports = router;
