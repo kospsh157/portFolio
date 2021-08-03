f5ab704fbc597f3f3528dbcb28bf090ab08e5e02
SungHo Park <kospsh157@gmail.com>
2021-08-03 15:21:58 -0700

    non critical fix

--- reactFront/src/components/ErrComponents/Err404Page.jsx
+++ reactFront/src/components/ErrComponents/Err404Page.jsx
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
 import BodyStyled from '../BodyStyled.jsx';
-import {useHistory, useParams} from 'react-router-dom';
 
 const Err404PageStyled = styled(BodyStyled)`
-
 `;
 
 function Err404Page() {
-    
-   
 
     return (
         <Err404PageStyled>
--- reactFront/src/components/Square.jsx
+++ reactFront/src/components/Square.jsx
@@ -2,15 +2,6 @@ import styled from 'styled-components';
 
 // create basic component;
 const Square = styled.div`
-    /* width: ${props => props.width};
-    height: ${props => props.height};
-    padding: ${props => props.padding || 0};
-    margin: ${props => props.margin || 0};
-    border-left: ${props => props.borderLeft || 0} solid black;
-    border-top: ${props => props.borderTop || 0} solid black;
-    border-right: ${props => props.borderRight || 0} solid black;
-    border-bottom: ${props => props.borderBottom || 0} solid black;
-    float: ${props => props.float || 'none'};     */
     box-sizing: border-box;
     background-color: rgba(240, 237, 221, 0.4);
     margin: 0;
